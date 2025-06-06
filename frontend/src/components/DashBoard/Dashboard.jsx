f73a504445d4e13e6fd15a9d57e26c8b16f31d1f
shreya188btcse23@igdtuw.ac.in <mukulkumar0811975@gmail.com>
2025-06-06 23:57:58 +0530

    added edi profile

--- frontend/src/components/DashBoard/Dashboard.jsx
+++ frontend/src/components/DashBoard/Dashboard.jsx
@@ -3,6 +3,20 @@ import { Link } from 'react-router-dom'
 import { User, PenLine, FileText, Menu, X } from 'lucide-react'
 import Navbar from '../Navbar/Navbar'
 import { Avatar, AvatarImage } from "@/components/ui/avatar"
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Textarea } from '../ui/textarea'
 export default function Dashboard() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false)
 
@@ -101,6 +115,43 @@ export default function Dashboard() {
                       <p className="font-medium">Bio:</p>
                       <p>CSE student at MNIT Jaipur, passionate about web development and security.</p>
                     </div>
+                    <Dialog>
+                      <form>
+                        <DialogTrigger asChild>
+                          <Button className='bg-pink-600 cursor-pointer'>Edit Profile</Button>
+                        </DialogTrigger>
+                        <DialogContent className="sm:max-w-[425px] bg-black text-white">
+                          <DialogHeader>
+                            <DialogTitle>Edit profile</DialogTitle>
+                            <DialogDescription className='text-white'>
+                              Make changes to your profile here. Click save when you&apos;re
+                              done.
+                            </DialogDescription>
+                          </DialogHeader>
+                          <div className="grid gap-4">
+                            <div className="grid gap-3">
+                              <Label htmlFor="name-1">First Name</Label>
+                              <Input id="name" name="firstName" placeholder="Your first name" />
+                            </div>
+                            <div className="grid gap-3">
+                              <Label htmlFor="username-1">Last Name</Label>
+                              <Input id="name" name="lastName" placeholder="Your last name" />
+                            </div>
+                            <div className="grid gap-3">
+                              <Label htmlFor="username-1">Email</Label>
+                              <Input id="email" name="email" placeholder="youremail@example.com" />
+                            </div>
+                            <div className="grid gap-3">
+                              <Label htmlFor="username-1">Bio</Label>
+                              <Textarea id="bio" name="bio" placeholder="Your bio" />
+                            </div>
+                          </div>
+                          <DialogFooter>
+                            <Button type="submit">Save changes</Button>
+                          </DialogFooter>
+                        </DialogContent>
+                      </form>
+                    </Dialog>
                   </div>
                 </div>
               </div>
