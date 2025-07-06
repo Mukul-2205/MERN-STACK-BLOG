a8ba1f092f21ae2d53c67fd2a0c22c1acb1e7fe0
shreya188btcse23@igdtuw.ac.in <mukulkumar0811975@gmail.com>
2025-07-07 01:08:48 +0530

    changed in allblogs

--- frontend/src/components/AllBlogs/AllBlogs.jsx
+++ frontend/src/components/AllBlogs/AllBlogs.jsx
@@ -1,34 +1,35 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import BlogCard from '@/components/BlogCard/BlogCard'; // adjust path if needed
+import BlogCard from '@/components/BlogCard/BlogCard'; // Adjust if needed
 import Navbar from '../Navbar/Navbar';
 import axiosInstance from '@/utils/axiosInstance';
 import { setBlog } from '@/Store/blogSlice';
 
 function AllBlogs() {
   const { user } = useSelector(state => state.auth);
-  const dispatch=useDispatch()
-  const {blog}=useSelector(state=>state.blog)
-  console.log(blog);
-  
+  const dispatch = useDispatch();
+  const { blog } = useSelector(state => state.blog);
 
-  useEffect(()=>{
-    const getAllPublishedBlogs=async()=>{
+  useEffect(() => {
+    const getAllPublishedBlogs = async () => {
       try {
-        const res=await axiosInstance.get('/blog/get-published-blogs',{withCredentials: true})
-        if(res.data.success){
-          dispatch(setBlog(res.data.blogs))
-        } else{
-          alert("Unable to fetch published blogs")
+        const res = await axiosInstance.get('/blog/get-published-blogs', {
+          withCredentials: true,
+        });
+
+        if (res.data.success) {
+          dispatch(setBlog(res.data.blogs));
+        } else {
+          alert("Unable to fetch published blogs");
         }
       } catch (error) {
         console.log(error);
-        alert("Error while fetching blogs!!")
+        alert("Error while fetching blogs!");
       }
-    }
+    };
 
-    getAllPublishedBlogs()
-  }, [])
+    getAllPublishedBlogs();
+  }, [dispatch]);
 
   return (
     <>
@@ -39,11 +40,23 @@ function AllBlogs() {
             Please login first to read blogs.
           </div>
         ) : (
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-            {blog.map((blog) => (
-              <BlogCard key={blog._id} blog={blog} />
-            ))}
-          </div>
+          <>
+            {Array.isArray(blog) && blog.length > 0 ? (
+              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+                {blog.map((item) => (
+                  <BlogCard key={item._id} blog={item} />
+                ))}
+              </div>
+            ) : blog === null || blog === undefined ? (
+              <div className="text-center text-lg font-medium text-gray-400 mt-10">
+                Loading blogs...
+              </div>
+            ) : (
+              <div className="text-center text-lg font-medium text-gray-400 mt-10">
+                No published blogs found.
+              </div>
+            )}
+          </>
         )}
       </div>
     </>
